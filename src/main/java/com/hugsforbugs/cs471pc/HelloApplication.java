bc65f8b18c18c73d7d7b9084aed9a708cf5f493e
zyad atef <zyadatefali12@gmail.com>
2024-12-08 00:47:15 +0200

    style: added a style sheet

--- src/main/java/com/hugsforbugs/cs471pc/HelloApplication.java
+++ src/main/java/com/hugsforbugs/cs471pc/HelloApplication.java
@@ -1,9 +1,12 @@
 package com.hugsforbugs.cs471pc;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -11,10 +14,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("Main-page.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Main-page.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(root);
+        String css = this.getClass().getResource("style.css").toExternalForm();
+        scene.getStylesheets().add(css);
         stage.setScene(scene);
+        stage.setTitle("Okaayyyyy less go");
+//        stage.setResizable(false);
+//        stage.setScene(scene);
         stage.show();
     }
 
--- src/main/java/com/hugsforbugs/cs471pc/HelloController.java
+++ src/main/java/com/hugsforbugs/cs471pc/HelloController.java
@@ -1,14 +1,18 @@
 package com.hugsforbugs.cs471pc;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+     @FXML
+     private Label welcomeText;
+    public void hello(ActionEvent e){
+        System.out.println("HOWDY");
     }
+
+
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
 }
