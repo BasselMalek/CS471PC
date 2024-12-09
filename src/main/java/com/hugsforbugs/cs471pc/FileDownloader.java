146c88687b5d7aba9e06fe42c4c9cc42850043c5
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-12-10 00:05:53 +0200

    feat: FileDownloader v0.1.

--- src/main/java/com/hugsforbugs/cs471pc/FileDownloader.java
+++ src/main/java/com/hugsforbugs/cs471pc/FileDownloader.java
@@ -1,41 +1,123 @@
 package com.hugsforbugs.cs471pc;
 
-import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 
 import java.io.*;
-import java.security.Security;
-import java.util.Arrays;
-import java.util.concurrent.Callable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 
-public class FileDownloader implements Callable<Boolean>{
-    String uri;
-    String dest;
-    Integer size;
-    FTPClient serverConn;
+public class FileDownloader implements Callable<int[]> {
+    final Integer ftpPort = 2121;
+    final Integer chunkSize = 8192;
+    URI sourceURI;
+    String destinationPath;
+    Long size;
+    ExecutorService segmentRunner;
+    ArrayList<Long> segmentOffsets;
+    ArrayList<Boolean> segmentStates;
     InputStream fileReader;
     OutputStream fileWriter;
-    Integer retryAttempts;
+    Integer retryAttempts = 5;
 
-    public FileDownloader(String uri, String dest) throws IOException {
+
+    public FileDownloader(String uri, String dest) throws IOException, URISyntaxException {
 //        anonClient = new FTPClient();
-        this.uri = uri;
-        this.dest = dest;
+        this.sourceURI = new URI(uri);
+        this.destinationPath = dest;
+        this.segmentRunner = Executors.newFixedThreadPool(3);
     }
 
     @Override
-    public Boolean call() throws IOException {
+    public int[] call() throws IOException, ExecutionException, InterruptedException {
 
+        FTPClient anonClient = new FTPClient();
+        anonClient.connect(this.sourceURI.getHost(), this.ftpPort);
+        if (!anonClient.login("anonymous", "")) {
+            return new int[]{0, 1};
+        }
+        try {
+            this.size = anonClient.mlistFile(this.sourceURI.getPath()).getSize();
+        } catch (NullPointerException e) {
+            throw new RuntimeException("Invalid URI");
+        }
+        if (new File("C://").getUsableSpace() < this.size) {
+            return new int[]{0, 1};
+        }
+        this.segmentOffsets = calculateOffsets();
+        Future<Boolean> segOne = this.segmentRunner.submit(new SegmentDownloader());
+        Future<Boolean> segTwo = this.segmentRunner.submit(new SegmentDownloader());
+        Future<Boolean> segThr = this.segmentRunner.submit(new SegmentDownloader());
+
+        this.segmentStates = new ArrayList<>(List.of(segOne.get(), segTwo.get(), segThr.get()));
+        while ((this.segmentStates.contains(false))&this.retryAttempts>0){
+            if
+        }
+
+
+
+
+        return new int[]{0, 0};
+//        this.size = anonClient.mlistFile(this.sourceURI).getSize();
+//        if (new File(this.destinationURI).getUsableSpace() < this.size +)
+//
+//        if (fileStream == null) {
+//            throw new IOException("Failed to retrieve file: " + this.sourceURI);
+//        }
+//
+//        File destFile = new File(this.destinationURI);
+//        if (!destFile.exists() && !destFile.createNewFile()) {
+//            throw new IOException("Failed to create destination file: " + destinationURI);
+//        }
+//
+//        try (FileOutputStream result = new FileOutputStream(destFile);
+//             fileStream) {
+//            byte[] buffer = new byte[4096];
+//            int bytesRead;
+//            while ((bytesRead = fileStream.read(buffer)) != -1) {
+//                result.write(buffer, 0, bytesRead);
+//            }
+//            result.flush();
+//            fileStream.close();
+//        }
+//         if (!anonClient.completePendingCommand()) {
+//            throw new IOException("Failed to complete FTP command");
+//        }
+//
+//        System.out.println("Download complete. Server Reply: " + anonClient.getReplyString());
+//         return true;
     }
 
 
-    void pauseDownload(){};
+    void pauseDownload() {
+    }
 
-    void launchSegment(){};
+    ;
 
-    void suspendSegment(){};
+    void launchSegment() {
+    }
 
-    void retrySegment(){};
+    ;
+
+    void suspendSegment() {
+    }
+
+    ;
+
+    void retrySegment() {
+    }
+
+    ;
+
+    private ArrayList<Long> calculateOffsets() {
+        ArrayList<Long> calculatedResult = new ArrayList<>();
+        calculatedResult.set(0, 0L);
+        calculatedResult.set(1, Math.ceilDiv(this.size, 3) - 1);
+        calculatedResult.set(1, Math.ceilDiv(this.size, 3) * 2 - 1);
+        return calculatedResult;
+    }
 
 }
--- src/main/java/com/hugsforbugs/cs471pc/SegmentDownloader.java
+++ src/main/java/com/hugsforbugs/cs471pc/SegmentDownloader.java
@@ -5,4 +5,9 @@ import java.util.concurrent.Callable;
 public class SegmentDownloader implements Callable<Boolean> {
 
 
+    @Override
+    public Boolean call() throws Exception {
+        Thread.sleep(7000);
+        return true;
+    }
 }
