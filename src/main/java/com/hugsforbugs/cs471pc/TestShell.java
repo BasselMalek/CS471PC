f47022643d9c3dc4615a6eb0e5bf0a56dba99c83
zyad atef <zyadatefali12@gmail.com>
2024-12-10 23:34:46 +0200

    fix:resolved issues with add button

--- src/main/java/com/hugsforbugs/cs471pc/TestShell.java
+++ src/main/java/com/hugsforbugs/cs471pc/TestShell.java
@@ -1,23 +1,23 @@
-//package com.hugsforbugs.cs471pc;
-//
-//import java.io.IOException;
-//import java.net.URISyntaxException;
-//import java.util.concurrent.*;
-//
-//public class TestShell {
-//    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException, URISyntaxException {
-//        ThreadPoolExecutor runner = new ThreadPoolExecutor(8, 16, 10000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
-////       System.out.println(runner.submit(new FileDownloader("./test-files/test.txt", "./downloads/text.txt")));
-//       System.out.println(runner.submit(new FileDownloader("http://127.0.0.1/test-files/file.txt", "./downloads/")));
-////       System.out.println(runner.submit(new FileDownloader("http://127.0.0.1/test-files/lo.e", "./downloads/")));
-////       System.out.println(runner.submit(new FileDownloader("http://127.0.0.1/test-files/testimg-1.jpg", "./downloads/")));
-////       System.out.println(runner.submit(new FileDownloader("http://127.0.0.1/test-files/testimg-1.jpg", "./downloads/")));
-////       System.out.println(runner.submit(new FileDownloader("./test-files/testpdf.pdf", "./downloads/testpdf.pdf")));
-//        runner.shutdown();
-//        runner.awaitTermination(1, TimeUnit.MINUTES);
-////        d.startDownload("./test-files/test.txt", "./downloads/text.txt");
-////        d.startDownload("./test-files/testimg-1.jpg", "./downloads/img1.jpg");
-////        d.startDownload("./test-files/testimg-2.png", "./downloads/img2.png");
-////        d.startDownload("./test-files/testpdf.pdf", "./downloads/pdf.pdf");
-//    }
-//}
+package com.hugsforbugs.cs471pc;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.concurrent.*;
+
+public class TestShell {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException, URISyntaxException {
+        ThreadPoolExecutor runner = new ThreadPoolExecutor(8, 16, 10000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+//       System.out.println(runner.submit(new FileDownloader("./test-files/test.txt", "./downloads/text.txt")));
+       System.out.println(runner.submit(new FileDownloader("http://127.0.0.1/test-files/file.txt", "./downloads/")));
+//       System.out.println(runner.submit(new FileDownloader("http://127.0.0.1/test-files/lo.e", "./downloads/")));
+//       System.out.println(runner.submit(new FileDownloader("http://127.0.0.1/test-files/testimg-1.jpg", "./downloads/")));
+//       System.out.println(runner.submit(new FileDownloader("http://127.0.0.1/test-files/testimg-1.jpg", "./downloads/")));
+//       System.out.println(runner.submit(new FileDownloader("./test-files/testpdf.pdf", "./downloads/testpdf.pdf")));
+        runner.shutdown();
+        runner.awaitTermination(1, TimeUnit.MINUTES);
+//        d.startDownload("./test-files/test.txt", "./downloads/text.txt");
+//        d.startDownload("./test-files/testimg-1.jpg", "./downloads/img1.jpg");
+//        d.startDownload("./test-files/testimg-2.png", "./downloads/img2.png");
+//        d.startDownload("./test-files/testpdf.pdf", "./downloads/pdf.pdf");
+    }
+}
